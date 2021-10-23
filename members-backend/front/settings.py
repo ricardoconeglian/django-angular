04ebe3bae37c1630adc7238fad49bca77717a3a0
ricardoconeglian <72042399+ricardoconeglian@users.noreply.github.com>
2021-10-23 12:24:55 -0300

    Acertando politica de CORS

--- members-backend/front/settings.py
+++ members-backend/front/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     
     "http://localhost:4200",
-    "http://35.247.197.251:80"
+    "http://35.247.197.251:8000"
    
 ]
 
